a3267827695902a63520d4b275abd26044d432e9
heyong.wd <heyong.wd@alibaba-inc.com>
2018-05-29 15:54:32 +0800

    band Name Generator init

--- main/arr_base.go
+++ main/arr_base.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 /* PositiveSum */
 func PositiveSum(numbers []int) int {
 
@@ -7,10 +12,62 @@ func PositiveSum(numbers []int) int {
 		return 0
 	}
 	var sum = 0
-	for i := 0; i < len(numbers); i++ {
-		if numbers[i] > 0 {
-			sum = sum + numbers[i]
+	for _, i := range numbers {
+		if i > 0 {
+			sum += i
 		}
 	}
 	return sum
+}
+
+func bandNameGenerator1(word string) string {
+	first := word[:1]
+	last := word[len(word)-1:]
+
+	if first != last {
+		return "The " + strings.Title(word)
+	}
+
+	return strings.Title(word) + word[1:]
+}
+
+/**
+ * 复杂版本
+ */
+func BandNameGenerator(word string) string {
+	// chars := []rune(word)
+	finalStr := ""
+	if word[0] == word[len(word)-1] {
+		finalStr := splitAndUpper(word)
+		return finalStr
+	} else {
+		finalStr := titleAndThe(word)
+		return finalStr
+	}
+	// strs
+	// strings.to
+	// if {
+
+	// }
+
+	return finalStr
+}
+
+func splitAndUpper(word string) string {
+	finalStr := word
+	r := []rune(finalStr)
+	for i := 0; i < len(r); i++ {
+		fmt.Println("r[", i, "]=", r[i], "string=", string(r[i]))
+		finalStr += string(r[i])
+	}
+	// for i := 1; i < len(strs); i++ {
+	// 	finalStr += strs[i]
+	// }
+	return strings.Title(finalStr)
+}
+
+func titleAndThe(word string) string {
+	finalStr := "the "
+	finalStr += word
+	return strings.Title(finalStr)
 }
--- main/main.go
+++ main/main.go
@@ -7,6 +7,13 @@ import (
 func main() {
 	fmt.Println("test")
 	// PositiveSum(nil)
-
-	PositiveSum(nil)
+	// PositiveSum([]int{1, 2, 3})
+	// fmt.Println(BandNameGenerator("wangd"))
+	// PositiveSum([]int{2, 33, 44})
+	// PositiveSum(nil)o
+	str := "1213"
+	fmt.Println(str[:3])
+	// chars := []rune(str)
+	// fmt.Println(chars[0] == chars[len(chars)-1])
+	// fmt.Println(BandNameGenerator("alaska"))
 }
